dc6a25a8d09e8801734f478a47c85c150c58b598
Reyshal <reyke.svb@gmail.com>
2024-11-10 20:28:52 +0700

    feat: add template for joining a room for refactoring later

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-
+import AgoraRTC, { AgoraRTCProvider } from "agora-rtc-react";
 import App from "./App";
-
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <AgoraRTCProvider client={client}>
+        <App />
+      </AgoraRTCProvider>
+    </StrictMode>
+  </Provider>
 );
