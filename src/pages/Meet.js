40764d083be35bc2ed4ab9292d989e0f051b4d69
Reyshal <reyke.svb@gmail.com>
2024-11-11 09:28:48 +0700

    style: cleaning up codes

--- src/pages/Meet.js
+++ src/pages/Meet.js
@@ -3,18 +3,17 @@ import { useSelector } from "react-redux";
 
 const Meet = () => {
   const { channel, username } = useSelector((state) => {
-    console.log(state);
     return {
       channel: state.room.channel,
       username: state.room.username,
     };
   });
 
-  // useEffect(() => {
-  //   if (!channel || !username) {
-  //     window.location.href = `/`;
-  //   }
-  // }, [channel, username]);
+  useEffect(() => {
+    if (!channel || !username) {
+      window.location.href = `/`;
+    }
+  }, [channel, username]);
 
   return (
     <div>
